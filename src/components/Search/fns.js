311762e3bdba55a281f6f06dfe739e8664278d1f
Pokesi <81540683+Pokesi@users.noreply.github.com>
2022-09-02 12:04:46 +1000

    Update fns.js

--- src/components/Search/fns.js
+++ src/components/Search/fns.js
@@ -923,7 +923,7 @@ export const abi = [
   ];
 
 export const overrides = {
-  value: utils.parseEther("0")
+  value: utils.parseEther("5")
 }
 
 export const externalAbi = [{"inputs":[{"internalType":"address","name":"_rave","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"string","name":"name","type":"string"},{"indexed":false,"internalType":"string","name":"key","type":"string"},{"indexed":false,"internalType":"string","name":"value","type":"string"}],"name":"SetText","type":"event"},{"inputs":[],"name":"RAVE_IDENTIFIER","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"name","type":"string"}],"name":"getRecords","outputs":[{"internalType":"string[]","name":"","type":"string[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"name","type":"string"},{"internalType":"string","name":"key","type":"string"}],"name":"getText","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"rave","outputs":[{"internalType":"contract IRaveBase","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"name","type":"string"},{"internalType":"string","name":"key","type":"string"},{"internalType":"string","name":"value","type":"string"}],"name":"setText","outputs":[],"stateMutability":"nonpayable","type":"function"}];
